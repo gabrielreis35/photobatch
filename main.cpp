031456c3cacab37bff1b3721401fcfa2e5a5914c
Gabriel Reis <gabrielreis@vixdynamics.com.br>
2023-11-07 15:20:24 -0300

    update project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
+#include <map>
 
-using namespace std;
+class ArgumentParser {
+public:
+    void RegisterFlag(const std::string& flag) {
+        if(!flag.empty()) {
+            map_Flags[flag] = false;
+        }
+    }
 
-int main() {
+    bool GetFlag(const std::string& flag) const {
+        if(!flag.empty()) {
+            auto flagIt = map_Flags.find(flag);
+            if(flagIt != std::end(map_Flags)) {
+                return flagIt->second;
+            }
+        }
+        return false;
+    }
+
+    void Parse(int argc, char* argv[]) {
+        if(argc > 1) {
+            for(int i = 1; i < argc; ++i) {
+                std::string arg = argv[i];
+
+                // verify arg contain -...'
+                if(arg.length() >= 3) {
+                    if(arg[0] == '-') {
+                        arg = arg.substr(1);
+                        if(arg.find_first_of('=')) {
+
+                        }
+                        else {
+                            auto flagIt = map_Flags.find(arg);
+                            if(flagIt != std::end(map_Flags)) {
+                                flagIt->second = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+private:
+    std::map<std::string, bool> map_Flags;
+};
+
+
+int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "pt_BR");
-    std::cout << "Hello, World!" << std::endl;
+
+    ArgumentParser argParser;
+    argParser.RegisterFlag("rename");
+    argParser.RegisterFlag("convert");
+    argParser.RegisterFlag("resize");
+    argParser.RegisterFlag("scale");
+
+    argParser.Parse(argc, argv);
+
+    std::cout << std::boolalpha << "Rename: " << argParser.GetFlag("rename") << std::endl;
+    std::cout << std::boolalpha << "Convert: " << argParser.GetFlag("convert") << std::endl;
+    std::cout << std::boolalpha << "Resize: " << argParser.GetFlag("resize") << std::endl;
+    std::cout << std::boolalpha << "Scale: " << argParser.GetFlag("scale") << std::endl;
     return 0;
 }
